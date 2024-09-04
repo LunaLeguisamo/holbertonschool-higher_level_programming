3b6cb812c989716e1ee4cce70d2639988efada28
LunaLeguisamo <9356@holbertonstudents.com>
2024-09-04 18:46:03 +0000

    6-print_comb3.py codestyle

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 
 for n in range(0, 10):
     for i in range(n + 1, 10):
-        print("{}{}" .format(n,i), end=", ")
         if n == 8 and i == 9:
-            print("{}{}" .format(n,i))
+            print("{}{}" .format(n, i))
+        else:
+            print("{}{}" .format(n, i), end=", ")
