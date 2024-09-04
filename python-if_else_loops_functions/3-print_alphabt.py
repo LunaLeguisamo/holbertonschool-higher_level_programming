67b5118ee50f39a8fb0c7ab17b6434ddb5bbfda2
LunaLeguisamo <9356@holbertonstudents.com>
2024-09-04 17:06:40 +0000

    codestyle

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@
 for letter in range(97, 123):
     if letter == 113 or letter == 101:
         continue
-        print("{}" .format(chr(letter)), end="")
+    print("{}" .format(chr(letter)), end="")
