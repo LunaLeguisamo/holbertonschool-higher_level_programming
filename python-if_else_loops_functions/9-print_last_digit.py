3f6ec72ccd52e3e5de8ca2fbce3305153887d935
LunaLeguisamo <9356@holbertonstudents.com>
2024-09-05 00:48:00 +0000

    9-print_last_digit

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def print_last_digit(number):
+    if number < 0:
+        number = -number
+    else:
+        number = number
+    last = number % 10
+    print("{}" .format(last), end="")
