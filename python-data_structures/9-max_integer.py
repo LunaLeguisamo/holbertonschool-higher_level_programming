509c6d0775e2ab6ad004408684b1ff41a0485bf6
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-11 14:23:38 +0000

    9-max_integer

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        for number in my_list[0:]:
+            for i in range(len(my_list)):
+                if my_list[i] > number:
+                    return my_list[i]
