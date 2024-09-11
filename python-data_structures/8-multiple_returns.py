40385bd0bd1cd07aa446829f7c89a95e02177f95
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-11 14:05:16 +0000

    8-multiple_returns fixed

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,10 +2,11 @@
 
 def multiple_returns(sentence):
     if sentence == "":
-        a = None
+        a = "None"
         b = 0
+        tupla = (b, a)
     else:
         a = sentence[0]
         b = len(sentence)
         tupla = (b, a)
-        return tupla
+    return tupla
