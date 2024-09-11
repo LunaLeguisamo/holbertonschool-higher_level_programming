feac0c6ae8d91c61c0d4e040f8e0877575c714cc
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-11 17:56:51 +0000

    9-max_integer negatives

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        i = 0
+        i = my_list[0]
         for number in my_list[0:]:
             if number > i:
                 i = number
