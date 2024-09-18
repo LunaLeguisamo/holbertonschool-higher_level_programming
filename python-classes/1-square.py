ded9f0f743ed3f8c08983df94a9b9e935a5c051d
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-18 00:52:31 +0000

    1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class Square.
+"""
+
+
+class Square:
+    """
+    An class that defines a square.
+    """
+    def __init__(self, size):
+        self.__size = size
