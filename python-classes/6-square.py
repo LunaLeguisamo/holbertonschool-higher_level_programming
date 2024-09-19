adf93d501909a2481c018d3b5eeeb1c26727d87b
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-19 19:35:22 +0000

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -64,7 +64,5 @@ class Square:
         else:
             if self.__position[1] > 0:
                 print()
-            else:
-                print(" ")
         for i in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
