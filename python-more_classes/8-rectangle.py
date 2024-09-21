46673d55e765973d1d3196db8b1d7418e8c517ed
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-21 17:53:34 +0000

    8-rectangle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -60,6 +60,18 @@ class Rectangle:
         else:
             return 2 * (self.__width + self.__height)
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() == rect_2.area():
+            return rect_1
+        if rect_1.area() > rect_2.area():
+            return rect_1
+        rect_2
+
     def __str__(self):
         a = ""
         if self.__width == 0 or self.__height == 0:
