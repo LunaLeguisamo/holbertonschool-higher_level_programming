1be2da817ae14fdfa6f70327da28746b4645193f
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-26 15:50:41 -0300

    2-is_same_class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module defines a class MyList with inheritance of list.
+"""
+
+
+def is_same_class(obj, a_class):
+    if isinstance(obj, a_class):
+        return True
