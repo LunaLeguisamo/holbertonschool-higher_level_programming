99260e8a7fd1be6ca62e75cd128bab1c5ffd6608
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-26 15:56:03 -0300

    22-is_same_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,5 +6,8 @@ instance of the specified class ; otherwise False.
 
 
 def is_same_class(obj, a_class):
+    """
+    Check if the object is exactly an instance of the specified class.
+    """
     if isinstance(obj, a_class):
         return True
