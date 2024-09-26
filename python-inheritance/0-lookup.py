12387d407d532cb73ec11d88db9a4b81c5aba207
Luna Leguisamo <9356@holbertonstudents.com>
2024-09-26 14:35:43 -0300

    0-lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-
+"""
+This module defines a class lookup.
+"""
 def lookup(obj):
+    """
+    Return a list of the attributes and methods of an object.
+    """
     return dir(obj)
