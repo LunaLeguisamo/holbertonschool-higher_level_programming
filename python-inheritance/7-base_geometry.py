a907f3cfb42c1441515977b71dc5aa948f4d46c7
LunaLeguisamo <9356@holbertonstudents.com>
2024-09-27 20:31:52 -0300

    7-base_geometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,7 +11,7 @@ class BaseGeometry:
 
     def area(self):
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         self.name = value
         if not isinstance(value, int):
