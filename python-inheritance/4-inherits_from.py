9fd7da750a131751ed42715442eaba417dc37ee2
LunaLeguisamo <9356@holbertonstudents.com>
2024-09-27 18:48:12 -0300

    5

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
     inherited (directly or indirectly)
     """
 
-    return (isinstance(obj, a_class) and type(obj) is not a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
