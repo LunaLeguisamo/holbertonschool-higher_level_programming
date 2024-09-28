361692a56db674390563f735d08738d7f5938cdf
LunaLeguisamo <9356@holbertonstudents.com>
2024-09-27 21:44:56 -0300

    8-rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,24 +1,51 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
-This is a module
+This module defines a Rectangle class that inherits from BaseGeometry.
+The Rectangle class represents a rectangle defined by its width and height,
+both of which must be positive integers.
 """
 
 
 class Rectangle(BaseGeometry):
     """
-    Class Rectangle that represents a rectangle, inheriting from BaseGeometry.
+    Class Rectangle that inherits from BaseGeometry.
+
+    This class represents a rectangle and includes validation for its width
+    and height. The rectangle is initialized with a specific width and height,
+    which must be positive integers. The class leverages the integer_validator
+    method from the BaseGeometry class to ensure the integrity of these values.
+
+    Attributes:
+        __width (int): The width of the rectangle. It must be a
+        positive integer.
+        __height (int): The height of the rectangle. It must also
+        be a positive integer.
     """
     def __init__(self, width, height):
         """
-        Initialize a new Rectangle instance.
+        Initialize a new instance of Rectangle.
 
-        Validates the width and height using the integer_validator
-        method from BaseGeometry.
+        The constructor accepts two parameters, width and height, which are
+        validated using the integer_validator method from the BaseGeometry
+        class.
+        If the validation fails, appropriate exceptions are raised.
+
+        Args:
+            width (int): The width of the rectangle. Must be a positive
+            integer.
+            height (int): The height of the rectangle. Must be a positive
+            integer.
 
         Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than or equal to zero.
+            TypeError: If either width or height is not an integer.
+            ValueError: If either width or height is less than or
+            equal to zero.
+
+        Example:
+            >>> r = Rectangle(3, 5)
+            >>> print(r)
+            <Rectangle object at ...>
         """
         super().integer_validator("width", width)
         self.__width = width
