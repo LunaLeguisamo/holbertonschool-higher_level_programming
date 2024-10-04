36143e8bfca32c1b77bbc3cd58bd35b535f386bc
Luna Leguisamo <9356@holbertonstudents.com>
2024-10-04 12:13:36 -0300

    2-append_write

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module define a function that appends a
+string at the end of a text file
+and return the number of characters added
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file
+     and return the number of characters added
+    """
+
+    with open(filename, "w") as f:
+        return f.write(text)
