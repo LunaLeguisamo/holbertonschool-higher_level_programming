2b6229269acfeff8cbf8b7ad76b44264ad2e8690
Luna Leguisamo <9356@holbertonstudents.com>
2024-10-04 11:30:28 -0300

    0-read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
+"""
+Este módulo proporciona una función para leer el contenido de un archivo y
+imprimirlo en la consola.
+"""
+
+
 def read_file(filename=""):
+    """
+    Lee el contenido del archivo especificado y lo imprime en la consola.
+    """
 
     with open(filename, "r") as myFile:
         print(myFile.read())
