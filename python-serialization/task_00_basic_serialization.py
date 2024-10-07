23493143f5f1298d9d35cdda97a27bd33fee645b
Luna Leguisamo <9356@holbertonstudents.com>
2024-10-06 21:56:02 -0300

    0-task_00_basic_serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -9,10 +9,9 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     with open(filename, "w") as file:
-        json.dump(data)
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     with open(filename, "r") as file:
-        f = json.load(file)
-        return f
+        return json.load(file)
