0e8236a4573b4ddf26e044f224f8ef2565a06750
Luna Leguisamo <9356@holbertonstudents.com>
2024-10-06 22:09:07 -0300

    task_01_pickle

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module that define a class
+
+"""
+
+import pickle
+
+
+class CustomObject:
+
+    def __init__(self, name=str, age=int, is_student=bool):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        with open(filename, "w") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "w") as file:
+            return pickle.load(file)
