355f443800a7856b2f03017490d3efbda51da67e
Luna Leguisamo <9356@holbertonstudents.com>
2024-10-10 16:44:06 +0000

    restful api

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Module that def a function
+
+"""
+import requests
+import json
+import csv
+
+
+def fetch_and_print_posts():
+    data = ""
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(r.status_code)
+
+    if r.status_code == 200:
+        data = r.json()
+
+        for d in data:
+            print(f"{d['title']}")
+
+
+def fetch_and_save_posts():
+    data = ""
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+
+    if r.status_code == 200:
+        data = r.json()
+
+        info = []
+        for d in data:
+            info.append({
+                'id': d['id'],
+                'title': d['title'],
+                'body': d['body']
+                })
+
+        with open("posts.csv", "w", newline="") as file:
+            csvR = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+
+            csvR.writeheader()
+            csvR.writerows(info)
