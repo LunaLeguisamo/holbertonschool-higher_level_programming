5e36a838528b7971bf3c738c456edb92ae1a2f16
Luna Leguisamo <9356@holbertonstudents.com>
2024-10-15 17:03:32 -0300

    task_05_basic_security.py

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -5,34 +5,38 @@ Module that def a function
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-
+import jwt
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
-jwt = JWTManager(app)
+#jwt = JWTManager(app)
 
 users = {
       "user1": {"username": "user1", "password": generate_password_hash("123"), "role": "user"},
       "admin1": {"username": "admin1", "password": generate_password_hash("678"), "role": "admin"}
       }
-
-@app.route("/data", methods=["POST", "GET"])
-def verify(username, password):
-    if username in users and check_password_hash(users['password'], password):
-        return users['username']
     
+secretKey = '10'
 @app.route("/login", methods=["POST"])
-@auth.login_required
+#@auth.login_required
 def login():
+    r = request.get_json()
+    for i in users:
+        if users[i]['username'] == r.get('username'):
+            user = users[i]
+            break
+    else:
+        return "not found", 404
+    if not check_password_hash(user['password'], r.get('password')):
+        return "wrong password", 403
     username = users.get('username')
     rol = users.get('rol')
     payload = {'usesrname': username, 'rol': rol}
-    secretKey = 'SuperSecretKey'
-    token = jwt.sign(payload, secretKey)
+    token = jwt.encode(payload, secretKey)
     return token
 
 @app.route("/basic-protected", methods=["GET"])
@@ -49,3 +53,8 @@ def prot():
 def admin():
     if users['role'] == 'admin':
         return "Admin Access: Granted"
+    else:
+        return jsonify({"error": "Admin access required"}), 403
+
+if __name__ == "__main__":
+    app.run(debug=True)
