8b3532543d2ed83f30dfd9f5035207d13b96a488
Luna Leguisamo <9356@holbertonstudents.com>
2024-10-30 15:57:09 -0300

    16-no_link

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0
 SELECT score, name
-WHERE name IS NOT null
 FROM second_table
-ORDER BY second_table DESC;
+WHERE name IS NOT null
+ORDER BY score DESC;
