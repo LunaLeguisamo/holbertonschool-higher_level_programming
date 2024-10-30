60626952f975b0d72443c2b646c7e2da4321c62c
Luna Leguisamo <9356@holbertonstudents.com>
2024-10-30 16:07:40 -0300

    16

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- Script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
-SELECT COUNT(*) FROM second_table
-WHERE score = number
-ORDER BY second_table DESC;
+SELECT score, COUNT(*) as count
+FROM second_table
+GROUP BY score
+ORDER BY count DESC;
