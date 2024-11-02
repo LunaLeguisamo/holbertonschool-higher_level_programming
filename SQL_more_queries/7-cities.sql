9799e3e7f801ad5e4cbc042d4c3ff5cd24525515
Luna Leguisamo <9356@holbertonstudents.com>
2024-11-02 16:07:29 -0300

    7-cities

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,8 @@
 -- Script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL,
+    state_id INT,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
