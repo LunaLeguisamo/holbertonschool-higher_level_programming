76fdbd836c13e6f953c91fe3db5df40a0b92619b
Luna Leguisamo <9356@holbertonstudents.com>
2024-11-03 14:02:56 -0300

    SQL_more_queries

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Script that lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title as title
 FROM tv_shows
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_genres ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY title ASC;
