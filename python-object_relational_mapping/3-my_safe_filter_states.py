cf4df69a6450d6e054637ec1f2002eedbf052e34
Luna Leguisamo <9356@holbertonstudents.com>
2024-11-03 16:57:22 -0300

    object_relational_mapping

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This module connects to a MySQL database and
+retrieves states matching a given name.
+"""
+
+
+import sys
+import MySQLdb
+
+
+def main():
+    """
+    Main function to fetch and display states matching the name.
+    """
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         password=password, database=database)
+
+    sql_query = "SELECT * FROM states WHERE BINARY name = '%s' ORDER BY id ASC"
+
+    cursor = db.cursor()
+    cursor.execute(sql_query, (name,))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
