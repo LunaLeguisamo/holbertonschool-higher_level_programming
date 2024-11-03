28d0d04423f1c9f13220de921dd97cc96187e75c
Luna Leguisamo <9356@holbertonstudents.com>
2024-11-02 22:16:26 -0300

    SQL_more_queries

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name, COUNT(*) as number_of_shows
+FROM tv_genres
+JOIN tv_shows ON tv_genres.id = tv_shows.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
