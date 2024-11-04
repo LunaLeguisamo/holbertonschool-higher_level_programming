1e9dab24f15b0a92ad97f4fc4991dad5ba601771
Luna Leguisamo <9356@holbertonstudents.com>
2024-11-03 21:08:32 -0300

    object_relational_mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -33,7 +33,7 @@ def main():
     cursor.execute(sql_query, (name,))
     cities = cursor.fetchall()
 
-    print(",".join(city[0] for city in cities))
+    print(", ".join(city[0] for city in cities))
 
     cursor.close()
     db.close()
