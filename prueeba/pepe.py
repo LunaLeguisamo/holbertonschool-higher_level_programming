757d45792c5b537803243fb5c0512d85eb027ae7
Luna Leguisamo <9356@holbertonstudents.com>
2024-11-26 23:59:08 -0300

    1

--- prueeba/pepe.py
+++ prueeba/pepe.py
@@ -1,15 +1,12 @@
-def main():
+with open("example.txt", "w") as file:
+        file.write("Hello, world!\n")
+        file.write("Welcome to file I/O in Python.\n")
 
-    file = "example.txt"
-    text = "```text Hello, world! 
-                Welcome to file I/O in Python.
-                ```"
-
-    with open(file, "w") as f:
-        f.write(text)
-        
-    with open(file, "r") as f:
-        f.readline()
+Reading from the file
+with open("example.txt", "r") as file:
+    print("Contents of 'example.txt':")
+    for line in file:
+        print(line, end="")
         
         #!/usr/bin/python3
 with open("words.txt", "w") as file:
